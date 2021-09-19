cdead90181f7fc9715efa879842420c8e64a6eee
Joshua <mramos29@ucol.mx>
2021-09-18 20:20:54 -0500

    Mostrar todos los productos añadido

--- inventario.js
+++ inventario.js
@@ -1,5 +1,72 @@
 export default class Inventario{
     constructor(){
         this._inventario = new Array();
+        this._table = document.querySelector("#table");
+    }
+
+    //añadir producto al vector
+    add(product){
+        let pos = this._find(product);
+        
+
+        if (pos>=0){
+            return false;
+        }
+        else{
+            this._inventario.push(product);
+            console.log(this._inventario);
+            return true;
+        }
+    }
+
+    //Encontrar el producto en el vector
+    _find(product){
+        let pos = this._inventario.findIndex((p) => {
+            if(p.getId() === product.getId()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        });
+        return pos;
+    }
+
+    showAll(){
+        this._table.innerHTML = ""
+        for(let i=0; i<this._inventario.length;i++){
+            let row = this._table.insertRow(-1);
+
+            let colId = row.insertCell(0);
+            let colName = row.insertCell(1);
+            let colQuantity = row.insertCell(2);
+            let colCost = row.insertCell(3);
+            let colTotal = row.insertCell(4);
+
+            colId.innerHTML = this._inventario[i].getId();
+            colName.innerHTML = this._inventario[i].getName();
+            colQuantity.innerHTML = this._inventario[i].getQuantity();
+            colCost.innerHTML = this._inventario[i].getCost();
+            colTotal.innerHTML = this._inventario[i].getTotal();
+        }
+    }
+    showInverse(){
+        this._table.innerHTML = "";
+        console.log(this._inventario.length)
+        for(let i=0; i<this._inventario.length;i++){
+            let row = this._table.insertRow(0);
+
+            let colId = row.insertCell(0);
+            let colName = row.insertCell(1);
+            let colQuantity = row.insertCell(2);
+            let colCost = row.insertCell(3);
+            let colTotal = row.insertCell(4);
+
+            colId.innerHTML = this._inventario[i].getId();
+            colName.innerHTML = this._inventario[i].getName();
+            colQuantity.innerHTML = this._inventario[i].getQuantity();
+            colCost.innerHTML = this._inventario[i].getCost();
+            colTotal.innerHTML = this._inventario[i].getTotal();
+        }
     }
 }
--- main.js
+++ main.js
@@ -3,7 +3,44 @@ import Producto from "./producto.js";
 
 class App{
     constructor(){
-        
+        //declaracion de botones
+       let btnAdd = document.querySelector("#btnAdd");
+       let btnShowAll = document.querySelector("#showAll");
+       let btnShowInverse = document.querySelector("#showInverse");
+        //funcionalidad de botones
+       btnAdd.addEventListener("click", this._addProduct)
+       btnShowAll.addEventListener("click", this._showAll)
+       btnShowInverse.addEventListener("click",this._showInverse)
+
+       this._inventario = new Inventario();
     }
+
+    _showAll = () => {
+        this._inventario.showAll()
+    }
+    _showInverse = () => {
+        this._inventario.showInverse();
+    }
+
+    _addProduct = () => {
+        let product = Producto.readForm();
+
+        if(!product){
+            alert("Todos los campos son requeridos")
+            return;
+        }
+
+        let added = this._inventario.add(product);
+
+        if(added === false){
+            alert("El producto ya se encuentra en el inventario")
+            return;
+        }
+
+        alert("Se ha añadido el producto al inventario")
+        return;
+    }
+
+    
 }
 new App;
